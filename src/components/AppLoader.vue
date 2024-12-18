c36e7ef9261e9cd5e3b9e239dc8f5251bfbb41ac
Vladimir <dvvinfo@mail.ru>
2024-12-18 12:35:23 +0300

    added Login page

---
+++ src/components/AppLoader.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="lds-dual-ring"></div>
+</template>
+
+<style lang="css">
+.lds-dual-ring {
+  /* change color here */
+  color: var(--gold);
+}
+.lds-dual-ring,
+.lds-dual-ring:after {
+  box-sizing: border-box;
+}
+.lds-dual-ring {
+  display: inline-block;
+  width: 40px;
+  height: 40px;
+}
+.lds-dual-ring:after {
+  content: ' ';
+  display: block;
+  width: 34px;
+  height: 34px;
+  margin: 8px;
+  border-radius: 50%;
+  border: 6.4px solid currentColor;
+  border-color: currentColor transparent currentColor transparent;
+  animation: lds-dual-ring 1.2s linear infinite;
+}
+@keyframes lds-dual-ring {
+  0% {
+    transform: rotate(0deg);
+  }
+  100% {
+    transform: rotate(360deg);
+  }
+}
+</style>
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,5 +1,7 @@
+// router/index.ts
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import { useAuthStore } from '@/stores/auth'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -8,38 +10,63 @@ const router = createRouter({
       path: '/',
       name: 'home',
       component: HomeView,
+      meta: { requiresAuth: true },
+    },
+    {
+      path: '/login',
+      name: 'login',
+      component: () => import('../views/LoginView.vue'),
     },
     {
       path: '/logs',
       name: 'logs',
       component: () => import('../views/LogsView.vue'),
+      meta: { requiresAuth: true },
     },
     {
       path: '/backups',
       name: 'backups',
       component: () => import('../views/BackupsViews.vue'),
+      meta: { requiresAuth: true },
     },
     {
       path: '/users',
       name: 'users',
       component: () => import('../views/UsersView.vue'),
+      meta: { requiresAuth: true },
     },
     {
       path: '/reference',
       name: 'reference',
       component: () => import('../views/ReferenceView.vue'),
+      meta: { requiresAuth: true },
     },
     {
       path: '/reports',
       name: 'reports',
       component: () => import('../views/ReportsViews.vue'),
+      meta: { requiresAuth: true },
     },
     {
       path: '/settings',
       name: 'settings',
       component: () => import('../views/SettingsView.vue'),
+      meta: { requiresAuth: true },
     },
   ],
+})
+
+router.beforeEach((to, from, next) => {
+  const authStore = useAuthStore()
+  if (to.matched.some(record => record.meta.requiresAuth)) {
+    if (!authStore.isAuthenticated) {
+      next({ name: 'login' })
+    } else {
+      next()
+    }
+  } else {
+    next()
+  }
 })
 
 export default router
