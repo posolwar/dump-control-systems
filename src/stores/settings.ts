ada8bf10df9a0fe7116ce55010395c8cdc5feebd
Vladimir <dvvinfo@mail.ru>
2024-12-21 17:23:46 +0300

    fixed setting-page

--- src/stores/settings.ts
+++ src/stores/settings.ts
@@ -1,11 +1,12 @@
 import { defineStore } from 'pinia'
 import { ref } from 'vue'
 import axios from 'axios'
-import type { ISetting } from '@/types/setting'
+import type { ISetting, ISettingOption } from '@/types/setting'
 import { useAuthStore } from '@/stores/auth'
 
 export const useSettingsStore = defineStore('settings', () => {
   const settings = ref<ISetting[]>([])
+  const settingsOptions = ref<ISettingOption[]>([])
   const settingsLoading = ref<boolean>(false)
   const settingsError = ref<string | null>(null)
   const tgToken = ref<string | null>(null)
@@ -21,7 +22,63 @@ export const useSettingsStore = defineStore('settings', () => {
         },
       })
       settings.value = response.data
-      localStorage.setItem('settings', JSON.stringify(settings.value))
+      localStorage.setItem('settings', JSON.stringify(settingsOptions.value))
+      console.log(settingsOptions.value)
+    } catch (error) {
+      console.error('Error fetching settings:', error)
+      settingsError.value = (error as Error).message
+    } finally {
+      settingsLoading.value = false
+    }
+  }
+  const loadSettingsFromLocalStorage = () => {
+    const storedSettings = localStorage.getItem('settings')
+    if (storedSettings) {
+      settings.value = JSON.parse(storedSettings)
+    }
+  }
+  const fetchSettingsOptions = async () => {
+    try {
+      settingsLoading.value = true
+      const response = await axios.get<ISettingOption[]>(
+        `${import.meta.env.VITE_BASE_URL}settings/options`,
+        {
+          headers: {
+            Authorization: `Bearer ${authStore.token}`,
+          },
+        },
+      )
+      settingsOptions.value = response.data
+      localStorage.setItem('settingsOptions', JSON.stringify(settingsOptions.value))
+      console.log(settingsOptions.value)
+    } catch (error) {
+      console.error('Error fetching settings:', error)
+      settingsError.value = (error as Error).message
+    } finally {
+      settingsLoading.value = false
+    }
+  }
+
+  const loadSettingsOptionsFromLocalStorage = () => {
+    const storedSettings = localStorage.getItem('settingsOptions')
+    if (storedSettings) {
+      settingsOptions.value = JSON.parse(storedSettings)
+    }
+  }
+  const fetchSettingsCreate = async (tgToken: string) => {
+    try {
+      settingsLoading.value = true
+      const response = await axios.post<ISetting[]>(
+        `${import.meta.env.VITE_BASE_URL}settings/new`,
+        { value: tgToken },
+        {
+          headers: {
+            Authorization: `Bearer ${authStore.token}`,
+          },
+        },
+      )
+      settings.value = response.data
+      localStorage.setItem('settingsO', JSON.stringify(settings.value))
       console.log(settings.value)
     } catch (error) {
       console.error('Error fetching settings:', error)
@@ -31,18 +88,11 @@ export const useSettingsStore = defineStore('settings', () => {
     }
   }
 
-  const loadSettingsFromLocalStorage = () => {
-    const storedSettings = localStorage.getItem('settings')
-    if (storedSettings) {
-      settings.value = JSON.parse(storedSettings)
-    }
-  }
-  const fetchSettingsCreate = async (tgToken: string) => {
+  const fetchDeleteSettings = async (settingName: string) => {
     try {
       settingsLoading.value = true
-      const response = await axios.post<ISetting[]>(
-        `${import.meta.env.VITE_BASE_URL}settings/new`,
-        { value: tgToken },
+      const response = await axios.delete<ISetting[]>(
+        `${import.meta.env.VITE_BASE_URL}settings/delete/${settingName}`,
         {
           headers: {
             Authorization: `Bearer ${authStore.token}`,
@@ -62,6 +112,7 @@ export const useSettingsStore = defineStore('settings', () => {
 
   return {
     settings,
+    settingsOptions,
     fetchSettings,
     loadSettingsFromLocalStorage,
     settingsLoading,
@@ -69,5 +120,8 @@ export const useSettingsStore = defineStore('settings', () => {
     fetchSettingsCreate,
     tgToken,
     sucssess,
+    fetchSettingsOptions,
+    loadSettingsOptionsFromLocalStorage,
+    fetchDeleteSettings,
   }
 })
